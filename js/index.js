9cd1abf826c46173f997cbd493b5780a9f9761c8
BurningFalls <sherlocky31415@gmail.com>
2024-02-28 21:08:15 +0900

    style: 개수 세는 방법 변경

--- js/index.js
+++ js/index.js
@@ -3,11 +3,15 @@ const registration_button = document.getElementById("registration");
 function showAllert() {
     const user_name = document.getElementById("name").value;
 
-    const query = 'input[type="checkbox"]:checked';
-    const selectedElements = document.querySelectorAll(query);
-    const selectedElementsCnt = selectedElements.length;
+    let cnt = 0;
+    for(let i = 1; i <= 5; i++) {
+        const str = "checkbox" + i;
+        if (document.getElementById(str).checked) {
+            cnt += 1;
+        }
+    }
 
-    window.alert(user_name + "님, 저와 " + selectedElementsCnt + "개의 취향이 같으시네요!");
+    window.alert(user_name + "님, 저와 " + cnt + "개의 취향이 같으시네요!");
 }
 
 registration_button.addEventListener("click", showAllert);
